1a6e2be76817417d504c41aab5eb9f6b0f1fdc25
chathuranga tennakoon <chathuranga@chathurangas-MacBook-Pro.local>
2020-06-09 22:39:36 +0900

    code refactored

--- src/main/java/com/example/springbootexperiment/transformer/ScreenResponseGenerator.java
+++ src/main/java/com/example/springbootexperiment/transformer/ScreenResponseGenerator.java
@@ -8,55 +8,46 @@ import com.example.springbootexperiment.response.Content;
 import com.example.springbootexperiment.response.HomeScreenData;
 import com.example.springbootexperiment.response.HomeScreenResponse;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ScreenResponseGenerator {
 
-    public static HomeScreenResponse generate(List<Screen> screens, List<CategoryContent> categoryContents,String menuId) {
-        List<HomeScreenData> homeScreenData = new ArrayList<>();
-        screens.forEach(screen -> {
-                    homeScreenData.add(
-                            HomeScreenData.builder()
-                                    .id(screen.getId())
-                                    .name(screen.getName())
-                                    .build()
+    public static HomeScreenResponse generate(List<Screen> screens, List<CategoryContent> categoryContents, String menuId) {
 
-                    );
-                }
-        );
-        Map<String, List<Content>> listMap = new HashMap<>();
-        categoryContents.forEach(categoryContent -> {
-            String categoryId = categoryContent.getCategory().getId();
-            if (listMap.get(categoryId) == null) {
-                listMap.put(categoryId, new ArrayList<Content>());
-            }
-            List<Content> list = listMap.get(categoryId);
-            list.add(Content.builder()
-                    .title(categoryContent.getContent().getTitle())
-                    .type(categoryContent.getContent().getType())
-//                    .images(categoryContent.getContent().getImages())
-                    .build());
-        });
-        List<Category> categories = new ArrayList<>();
+        long start = System.currentTimeMillis();
+        List<HomeScreenData> homeScreenData = screens.stream().map(screen ->
+                HomeScreenData.builder()
+                        .id(screen.getId())
+                        .name(screen.getName())
+                        .build()
+        ).collect(Collectors.toList());
 
-        categoryContents.forEach(categoryContent -> {
-            categories.add(
-                    Category.builder()
-                            .id(categoryContent.getCategory().getId())
-                            .name(categoryContent.getCategory().getName())
-                            .sequenceNo(categoryContent.getSequenceNo())
-                            .contents(listMap.get(categoryContent.getCategory().getId()))
-                            .build()
-            );
-        });
+        Optional<List<Category>> categoriesOptional = screens.parallelStream().map(screen ->
+                screen.getCategories()
+                        .stream()
+                        .map(category -> Category.builder().id(category.getCategory().getId())
+                                .name(category.getCategory().getName())
+                                .sequenceNo(category.getSequenceNumber()).contents(
+                                        categoryContents.parallelStream().filter(categoryContent ->
+                                                categoryContent.getCategory().getId().equals(category.getCategory().getId()))
+                                                .map(categoryContent ->
+                                                        Content.builder().type(categoryContent.getContent().getType())
+                                                                .title(categoryContent.getContent().getTitle())
+                                                                .build())
+                                                .collect(Collectors.toList())
+                                ).build()
+                        ).collect(Collectors.toList())
+        ).collect(Collectors.toList()).stream().findFirst();
+
         HomeScreenResponse homeScreenResponse = new HomeScreenResponse();
         HomeScreenData homeScreenData2 = homeScreenData.stream().filter(homeScreenData1 -> homeScreenData1.getId().equals(menuId)).findFirst().get();
-        homeScreenData2.setCategories(categories);
-        homeScreenData.add(homeScreenData2);
+        homeScreenData2.setCategories(categoriesOptional.get());
         homeScreenResponse.setData(homeScreenData);
+
+        long executionTime = System.currentTimeMillis() - start;
+        System.out.println("====time " + executionTime);
+
         return homeScreenResponse;
     }
 
